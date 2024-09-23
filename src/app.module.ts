204ba5e56aa720c5bf9e2911eda1181712cb5b8c
armandocodecr <totovip1464@gmail.com>
2024-09-23 15:10:37 -0600

    remove unnecessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { StellarContractModule } from './stellar-contract/stellar-contract.module';
 
 @Module({
   imports: [StellarContractModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
