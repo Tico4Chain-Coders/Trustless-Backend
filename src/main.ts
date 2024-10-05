6dd83503da560975464cbd0d18c150bcbd940b79
JoelVR17 <vermudo.com@gmail.com>
2024-10-05 13:57:33 -0600

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
 
   console.log("Now running in: " + process.env.PORT || 3000);
