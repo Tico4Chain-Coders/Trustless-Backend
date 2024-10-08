cf472b8cc03c37cc650e378f8d46e18ddc2f2ae2
armandocodecr <totovip1464@gmail.com>
2024-10-08 10:07:53 -0600

    chore: add new interface to escrow response

--- src/interfaces/response.interface.ts
+++ src/interfaces/response.interface.ts
@@ -1,6 +1,18 @@
-import * as StellarSDK from "@stellar/stellar-sdk";
+import * as StellarSDK from '@stellar/stellar-sdk';
 
 export interface ApiResponse {
-  status: StellarSDK.rpc.Api.GetTransactionStatus;
-  message: string;
+    status : StellarSDK.rpc.Api.GetTransactionStatus;
+    message: string;
+}
+
+export interface escrowResponse {
+    engagement_id   : string;
+    description     : string;
+    issuer          : string;
+    signer          : string;
+    service_provider: string;
+    amount          : number;
+    balance         : number;
+    cancelled       : boolean;
+    completed       : boolean;
 }
