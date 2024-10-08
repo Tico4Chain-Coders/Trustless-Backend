f7a98ca46037a9f6be3e7b9f1dd3553497479263
JoelVR17 <vermudo.com@gmail.com>
2024-10-07 23:18:07 -0600

    remove: unnecessary files

--- src/stellar-contract/engagement/engagement.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { EngagenmentController } from "./engagement.controller";
-import { EngagementService } from "./engagement.service";
-
-@Module({
-  controllers: [EngagenmentController],
-  providers: [EngagementService],
-})
-export class EngagementModule {}
