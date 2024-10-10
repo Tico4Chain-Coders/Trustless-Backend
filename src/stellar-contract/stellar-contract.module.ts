d56a897dddaed02f7f843fec23855cad0660b211
JoelVR17 <vermudo.com@gmail.com>
2024-10-10 13:03:26 -0600

    feat: hide user endpoints

--- src/stellar-contract/stellar-contract.module.ts
+++ src/stellar-contract/stellar-contract.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { EscrowModule } from "./escrow/escrow.module";
-import { UserModule } from "./user/user.module";
+// import { UserModule } from "./user/user.module";
 import { HelperModule } from "./helper/helper.module";
 
 @Module({
-  imports: [EscrowModule, UserModule, HelperModule],
+  imports: [EscrowModule /*, UserModule*/, HelperModule],
 })
 export class StellarContractModule {}
