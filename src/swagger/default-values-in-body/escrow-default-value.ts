009af77bb9856e3b7c9e290985dde3e7d5bfe127
JoelVR17 <vermudo.com@gmail.com>
2024-10-10 13:18:31 -0600

    feat: add default values into the body of swagger requests

---
+++ src/swagger/default-values-in-body/escrow-default-value.ts
@@ -0,0 +1,51 @@
+export const InitializeEscrowDefaultValue = {
+  default: {
+    value: {
+      engagementId: "",
+      description: "",
+      issuer: "",
+      serviceProvider: "",
+      amount: "",
+      signer: "",
+    },
+  },
+};
+
+export const FundEscrowDefaultValue = {
+  default: {
+    value: {
+      engamentId: "",
+      signer: "",
+      secretKey: "",
+    },
+  },
+};
+
+export const CompleteEscrowDefaultValue = {
+  default: {
+    value: {
+      engamentId: "",
+      signer: "",
+      secretKey: "",
+    },
+  },
+};
+
+export const CancelEscrowDefaultValue = {
+  default: {
+    value: {
+      engamentId: "",
+      signer: "",
+    },
+  },
+};
+
+export const RefundRemainingFundsDefaultValue = {
+  default: {
+    value: {
+      engamentId: "",
+      signer: "",
+      secretKey: "",
+    },
+  },
+};
