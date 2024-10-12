d570f509d0fbd6d5d2b68bc4b85e5e50ce039761
JoelVR17 <vermudo.com@gmail.com>
2024-10-11 20:35:01 -0600

    feat: add rate limit of calls in the API per client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from "@nestjs/common";
 import { StellarContractModule } from "./stellar-contract/stellar-contract.module";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-  imports: [StellarContractModule],
+  imports: [
+    StellarContractModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 50,
+      },
+    ]),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
