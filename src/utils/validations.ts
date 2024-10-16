2c0a8a92f74fbcf386c9a8dc44651613cb36f975
armandocodecr <totovip1464@gmail.com>
2024-10-16 14:13:46 -0600

    feat: create function to validate the format address

---
+++ src/utils/validations.ts
@@ -0,0 +1,11 @@
+import * as StellarSDK from '@stellar/stellar-sdk';
+
+
+export function validateAddress ( address ) {
+    try {
+        new StellarSDK.Address(address);
+        return true;
+      } catch (error) {
+        return false;
+      }
+}
