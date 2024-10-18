0ccf7fc896652972a4e28f5b9405ff6a21db9ab4
armandocodecr <totovip1464@gmail.com>
2024-10-17 22:38:38 -0600

    feat: add new dependencie to validate parameters

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as dotenv from "dotenv";
+import { ValidationPipe } from "@nestjs/common";
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
     origin: process.env.CLIENT_URL,
